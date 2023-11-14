ba428a1a4ef0d5caa1b325dfa52e11ad79a6a9e2
Michael <mail@michaelandrews.net>
2023-11-13 20:10:10 -0800

    Minor changes

--- create_subdirectories.py
+++ create_subdirectories.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python3
+
 import os
 import string
 
 # Set the path to your music directory here
-music_directory = '/path/to/your/music/folder'
+music_directory = '/Volumes/Music/All Music'
 
 # Create alphabetical subfolders and a folder for numeric filenames
 for letter in string.ascii_uppercase:
     os.makedirs(os.path.join(music_directory, letter), exist_ok=True)
 
 os.makedirs(os.path.join(music_directory, '0-9'), exist_ok=True)
+
+# Create an 'Other' folder for files that don't fit the other categories
+os.makedirs(os.path.join(music_directory, '_Other'), exist_ok=True)
 
 print("Subdirectories have been created.")
--- move_music_files.py
+++ move_music_files.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python3
+
 import os
 import shutil
 import string
 
 # Set the path to your music directory here
-music_directory = '/path/to/your/music/folder'
+music_directory = '/Volumes/Music/All Music'
 
 # Move files into the respective folders
 for filename in os.listdir(music_directory):
@@ -11,9 +13,16 @@ for filename in os.listdir(music_directory):
     
     if os.path.isfile(full_path):  # Check if it's a file
         first_char = filename[0].upper()
+        
+        # Determine the target folder
         if first_char in string.ascii_uppercase:
-            shutil.move(full_path, os.path.join(music_directory, first_char, filename))
+            target_folder = first_char
         elif first_char.isdigit():
-            shutil.move(full_path, os.path.join(music_directory, '0-9', filename))
+            target_folder = '0-9'
+        else:
+            target_folder = '_Other'
+
+        # Move the file to the target folder
+        shutil.move(full_path, os.path.join(music_directory, target_folder, filename))
 
 print("Music files have been organized.")
